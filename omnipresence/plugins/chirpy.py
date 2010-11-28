855ea003b141be8f7b3f1acb2cb9cc7d6c503e89
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2010-11-28 12:23:20 +0800

    chirpy: Add specific ID lookup and text search

--- omnipresence/plugins/chirpy.py
+++ omnipresence/plugins/chirpy.py
@@ -2,8 +2,11 @@ from zope.interface import implements
 from twisted.plugin import IPlugin
 from omnipresence.iomnipresence import ICommand
 
-import sqlobject
 import random
+import re
+import sqlobject
+
+ID_SYNTAX = re.compile(r'^#([0-9]+)$')
 
 class ChirpyCommand(object):
     implements(IPlugin, ICommand)
@@ -24,16 +27,30 @@ class ChirpyCommand(object):
         self.table = ChirpyQuote
 
     def execute(self, bot, user, channel, args):
-        quotes = self.table.select('CHAR_LENGTH(body) < 400 AND approved = 1')
+        args = args.split(None, 1)[1:]
+        
+        if args:
+            id_match = ID_SYNTAX.match(args[0])
+            if id_match:
+                id = int(id_match.group(1))
+                q_expression = sqlobject.AND(self.table.q.id==id,
+                                             self.table.q.approved==1)
+            else:
+                q_expression = sqlobject.AND(self.table.q.body.contains(args[0]),
+                                             self.table.q.approved==1)
+        else:
+            q_expression = 'CHAR_LENGTH(body) < 400 AND approved = 1'
+        
+        quotes = self.table.select(q_expression)
         if quotes.count() > 0:
             quote = random.choice(list(quotes))
             quote_text = quote.body.replace('\n', ' ')
-            bot.reply(user, channel, u'QDB: (#%d, %+d/%d) %s' % (quote.id,
-                                                                 quote.rating,
-                                                                 quote.votes,
-                                                                 quote_text))
+            bot.reply(user, channel, 'QDB: (#%d, %+d/%d) %s' % (quote.id,
+                                                                quote.rating,
+                                                                quote.votes,
+                                                                quote_text))
         else:
-            bot.reply(user, channel, u"QDB: Couldn't find any quotes!")
+            bot.reply(user, channel, "QDB: Couldn't find any quotes!")
 
 
 chirpycommand = ChirpyCommand()
