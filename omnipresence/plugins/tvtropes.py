5d14a6d26bf23398be98e9ee1caa09d494ab9f2e
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2011-03-31 16:17:17 -0400

    Rewrite of tvtropes module
    * Merge handler and command plugin classes
    
    * Work around TV Tropes bug regarding existence of articles

--- omnipresence/plugins/tvtropes.py
+++ omnipresence/plugins/tvtropes.py
@@ -3,6 +3,7 @@ import re
 import urllib
 
 from BeautifulSoup import BeautifulSoup
+from twisted.internet import threads
 from twisted.plugin import IPlugin
 from zope.interface import implements
 
@@ -29,38 +30,69 @@ def get_real_title_and_url(title, url):
 
 
 class TVTropesSearch(object):
-    def reply_with_trope(self, response, bot, prefix, reply_target, channel,
-                         args, info_text=''):
-        soup = BeautifulSoup(response[1])
-        (title, url) = get_real_title_and_url(html.textify_html(soup.find('title')),
-                                              response[0]['content-location'])
-        bot.reply(reply_target, channel, (u'TV Tropes%s: \x02%s\x02: %s'
-                                            % (info_text, title, url)) \
-                                          .encode(self.factory.encoding))
+    """
+    \x02%s\x02 \x1Fsearch_string\x1F - Search for a TV Tropes article with a 
+    title matching the given search string, or perform a full-text search if 
+    no such article exists.
+    """
+    implements(IPlugin, ICommand, IHandler)
+    name = 'tvtropes'
     
-    def reply_with_google(self, response, bot, prefix, reply_target, channel,
-                          args):
-        data = json.loads(response[1])
+    def execute(self, bot, prefix, reply_target, channel, args):
+        args = args.split(None, 1)
         
-        if ('responseData' not in data or
-            'results' not in data['responseData'] or
-            len(data['responseData']['results']) < 1):
-            bot.reply(prefix, channel, 'TV Tropes: No results found for '
-                                       '\x02%s\x02.' % args[1])
+        if len(args) < 2:
+            bot.reply(prefix, channel, 'Please specify a search string.')
             return
         
-        result = data['responseData']['results'][0]
-        (title, url) = get_real_title_and_url(html.decode_html_entities(result['titleNoFormatting']),
-                                              result['unescapedUrl'])
-        bot.reply(reply_target, channel, (u'TV Tropes (full-text): '
-                                          u'\x02%s\x02: %s' % (title, url)) \
-                                          .encode(self.factory.encoding))
+        d = threads.deferToThread(self.search, args[1])
+        d.addCallback(self.reply, bot, prefix, reply_target, channel, args)
+        return d
     
-    def find_trope(self, response, bot, prefix, reply_target, channel, args):
-        soup = BeautifulSoup(response[1])
+    def privmsg(self, bot, prefix, channel, message):
+        tropes = TROPE_LINK.findall(message)
         
-        while soup.find('a', 'twikilink'):
-            link = soup.find('a', 'twikilink').extract()
+        for match in tropes:
+            match = match.strip()
+            if match:
+                d = threads.deferToThread(self.search, match)
+                d.addCallback(self.reply_without_target, bot,
+                              prefix, channel, match)
+                return d
+    
+    action = privmsg
+    
+    def search(self, title):
+        # Start off by trying to find a trope with the exact title.
+        
+        # Transform the requested page title for {{}} search.  First,
+        # remove any characters invalid in plain trope titles, in order
+        # to increase the likelihood of an exact match.  Second, force
+        # the first character to uppercase; otherwise, an all-lowercase
+        # query will lead to the markup not being parsed as a link.
+        plain_title = INVALID_TROPE_TITLE_CHARACTERS.sub('', title)
+        plain_title = plain_title[0].upper() + plain_title[1:]
+        
+        # Work around a TV Tropes bug (2011-03-31) that returns "blue"
+        # existing article links instead of "red" nonexistent article
+        # links, by adding the known extant article "HomePage" to the
+        # beginning of the query.  When removing this workaround, make
+        # sure to update the link loop below as well!
+        preview_query = ('HomePage [=~%s~=] {{%s}}' % (title, plain_title))
+        params = {'source': preview_query}
+        
+        response = self.factory.get_http('http://tvtropes.org/pmwiki/preview.php',
+                                         'POST', body=urllib.urlencode(params),
+                                         headers={'Content-type':
+                                                  'application/x-www-form-urlencoded'},
+                                         defer=False)
+        preview_soup = BeautifulSoup(response[1])
+        
+        # Discard the bogus HomePage link.
+        preview_soup.find('a', 'twikilink').extract()
+        
+        while preview_soup.find('a', 'twikilink'):
+            link = preview_soup.find('a', 'twikilink').extract()
             
             try:
                 result = link.attrMap['href']
@@ -74,89 +106,77 @@ class TVTropesSearch(object):
             if result.rsplit('/', 1)[-1] == 'ptitle':
                 continue
             
-            d = self.factory.get_http(result)
-            d.addCallback(self.reply_with_trope, bot, prefix, reply_target, channel, args)
-            d.addErrback(bot.reply_with_error, prefix, channel, args[0])
-            return            
-        
+            # We've found our match.
+            response = self.factory.get_http(result, defer=False)
+            article_soup = BeautifulSoup(response[1])
+            return (html.textify_html(article_soup.find('title')),
+                    response[0]['content-location'], '')
+
+        # No results?  Try a full-text Google search.        
         params = urllib.urlencode({'q': ('site:tvtropes.org inurl:pmwiki.php '
                                          '-"click the edit button" %s,')
-                                          % args[1],
+                                          % title,
                                    'v': '1.0'})
         
-        d = self.factory.get_http('http://ajax.googleapis.com/ajax/services/search/web?' + params)
-        d.addCallback(self.reply_with_google, bot, prefix, reply_target, channel, args)
-        d.addErrback(bot.reply_with_error, prefix, channel, args[0])
-    
-    def check_trope(self, bot, prefix, reply_target, channel, args):
-        # Transform the requested page title for {{}} search.  First,
-        # remove any characters invalid in plain trope titles, in order
-        # to increase the likelihood of an exact match.  Second, force
-        # the first character to uppercase; otherwise, an all-lowercase
-        # query will lead to the markup not being parsed as a link.
-        plain_title = INVALID_TROPE_TITLE_CHARACTERS.sub('', args[1])
-        preview_query = ('[=~%s~=] {{%s%s}}'
-                          % (args[1], plain_title[0].upper(), plain_title[1:]))
-        params = {'source': preview_query}
+        response = self.factory.get_http('http://ajax.googleapis.com/ajax/'
+                                         'services/search/web?' + params,
+                                         defer=False)
+        data = json.loads(response[1])
         
-        d = self.factory.get_http('http://tvtropes.org/pmwiki/preview.php',
-                                  'POST', body=urllib.urlencode(params),
-                                  headers={'Content-type':
-                                           'application/x-www-form-urlencoded'})
-        d.addCallback(self.find_trope, bot, prefix, reply_target, channel, args)
-        return d
-
-
-class TVTropesSearchCommand(TVTropesSearch):
-    """
-    \x02%s\x02 \x1Fsearch_string\x1F - Search for a TV Tropes article with a 
-    title matching the given search string, or perform a full-text search if 
-    no such article exists.
-    """
-    implements(IPlugin, ICommand)
-    name = 'trope'
-    
-    def execute(self, bot, prefix, reply_target, channel, args):
-        args = args.split(None, 1)
+        if ('responseData' not in data or
+            'results' not in data['responseData'] or
+            not data['responseData']['results']):
+            return None
         
-        if len(args) < 2:
-            bot.reply(prefix, channel, 'Please specify a search string.')
+        result = data['responseData']['results'][0]
+        return (html.decode_html_entities(result['titleNoFormatting']),
+                result['unescapedUrl'], ' (full-text)')
+    
+    def reply(self, trope, bot, prefix, reply_target, channel, args):
+        if not trope:
+            bot.reply(prefix, channel, 'TV Tropes: No results found for '
+                                       '\x02%s\x02.' % args[1])
             return
         
-        return self.check_trope(bot, prefix, reply_target, channel, args)
+        (title, url, info_text) = trope
+        (title, url) = get_real_title_and_url(title, url)
+        
+        bot.reply(reply_target, channel, (u'TV Tropes%s: \x02%s\x02: %s'
+                                            % (info_text, title, url)) \
+                                          .encode(self.factory.encoding))
+    
+    def reply_without_target(self, trope, bot, prefix, channel, match):
+        if not trope:
+            bot.msg(channel, '\x0314TV Tropes: No results found for '
+                             '\x02%s\x02.' % match)
+            return
+        
+        (title, url, info_text) = trope
+        (title, url) = get_real_title_and_url(title, url)
+        
+        bot.msg(channel, (u'\x0314TV Tropes%s: \x02%s\x02: %s'
+                            % (info_text, title, url)) \
+                          .encode(self.factory.encoding))
 
 
-class RandomTropeCommand(TVTropesSearch):
+class RandomTrope(TVTropesSearch):
     """
     \x02%s\x02 - Get a random TV Tropes article.
     """
-    implements(IPlugin, ICommand)
-    name = 'trope_random'
+    name = 'tvtropes_random'
     
     def execute(self, bot, prefix, reply_target, channel, args):
-        d = self.factory.get_http('http://tvtropes.org/pmwiki/randomitem.php')
-        d.addCallback(self.reply_with_trope, bot, prefix, reply_target,
-                      channel, args, ' (random)')
+        d = threads.deferToThread(self.random)
+        d.addCallback(self.reply, bot, prefix, reply_target, channel, args)
         return d
-
-
-class TVTropesLinkHandler(TVTropesSearch):
-    """
-    Reply to messages containing {{trope links}} with the trope URL.
-    """
-    implements(IPlugin, IHandler)
-    name = 'tvtropes'
     
-    def privmsg(self, bot, prefix, channel, message):
-        tropes = TROPE_LINK.findall(message)
-        
-        for match in tropes:
-            if match.strip():
-                return self.check_trope(bot, prefix, channel, ['trope', match])
-    
-    action = privmsg
+    def random(self):
+        response = self.factory.get_http('http://tvtropes.org/pmwiki/randomitem.php',
+                                         defer=False)
+        soup = BeautifulSoup(response[1])
+        return (html.textify_html(soup.find('title')),
+                response[0]['content-location'], ' (random)')
 
 
-trope = TVTropesSearchCommand()
-trope_random = RandomTropeCommand()
-trope_handler = TVTropesLinkHandler()
+tvtropes = TVTropesSearch()
+tvtropes_random = RandomTrope()
