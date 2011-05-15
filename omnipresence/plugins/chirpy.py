3420f045f36cb67e2b241ab214f8ea658268d5a7
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2011-05-15 14:11:15 -0400

    chirpy: Add tag search and URL support (config change)

--- omnipresence/plugins/chirpy.py
+++ omnipresence/plugins/chirpy.py
@@ -8,13 +8,44 @@ from zope.interface import implements
 from omnipresence import util
 from omnipresence.iomnipresence import ICommand
 
+MAX_QUOTE_CHAR_LENGTH = 256
+MAX_QUOTE_BYTE_LENGTH = 384
+
 ID_SYNTAX = re.compile(r'^#([0-9]+)$')
+
+# An approximate Python version of the "get_search_instruction" function
+# in Chirpy/UI/WebApp.pm.  Originally contributed by James Kalenius.
+#
+# <https://bitbucket.org/kxz/omnipresence/issue/17>
+QUERY_REGEX = re.compile(r'"(.*?)"|"([^"]+)$|(\S+)')
+TAG_REGEX = re.compile(r'^tag:', re.IGNORECASE)
+
+def get_search_instruction(query):
+    queries = []
+    tags = []
+
+    m = QUERY_REGEX.findall(query)
+    for match in m:
+        if match[0]:
+            queries.append(match[0])
+        elif match[1]:
+            queries.append(match[1])
+        elif match[2]:
+            if TAG_REGEX.match(match[2]):
+                # Ensure that tag matches aren't blank.
+                t = TAG_REGEX.sub('', match[2])
+                if t:
+                    tags.append(t)
+            else:
+                queries.append(match[2])
+    return (queries, tags)
+
 
 class ChirpyCommand(object):
     """
     \x02%s\x02 [\x02#\x02\x1Fid\x1F|\x1Fsearch_string\x1F] - Retrieve a quote 
     from the quote database.  If neither a quote ID nor a search string is 
-    specified, return a random quote less than 400 characters long.
+    specified, return a random short quote.
     """
     implements(IPlugin, ICommand)
     name = 'chirpy'
@@ -24,39 +55,89 @@ class ChirpyCommand(object):
     def registered(self):
         sqlobject_uri = self.factory.config.get('chirpy', 'database')
         
+        self.connection = sqlobject.connectionForURI(sqlobject_uri)
+        self.prefix = self.factory.config.getdefault('chirpy', 'prefix', 'qdb_')
+        self.quote_url = self.factory.config.getdefault('chirpy', 'quote_url', '')
+        
         class ChirpyQuote(sqlobject.SQLObject):
-            _connection = sqlobject.connectionForURI(sqlobject_uri)
+            _connection = self.connection
             
             class sqlmeta:
                 fromDatabase = True
-                table = self.factory.config.get('chirpy', 'table')
+                table = self.prefix + 'quotes'
         
-        self.table = ChirpyQuote
+        self.quotes = ChirpyQuote
 
     def execute(self, bot, prefix, reply_target, channel, args):
-        args = args.split(None, 1)[1:]
+        args = args.split(None, 1)
         
-        if args:
-            id_match = ID_SYNTAX.match(args[0])
+        query = sqlobject.AND(sqlobject.func.CHAR_LENGTH(self.quotes.q.body)<256,
+                              self.quotes.q.approved==1)
+        
+        if len(args) > 1:
+            id_match = ID_SYNTAX.match(args[1])
             if id_match:
                 id = int(id_match.group(1))
-                q_expression = sqlobject.AND(self.table.q.id==id,
-                                             self.table.q.approved==1)
+                query = sqlobject.AND(self.quotes.q.id==id,
+                                      self.quotes.q.approved==1)
             else:
-                q_expression = sqlobject.AND(self.table.q.body.contains(args[0]),
-                                             self.table.q.approved==1)
-        else:
-            q_expression = 'CHAR_LENGTH(body) < 400 AND approved = 1'
+                (strings, tags) = get_search_instruction(args[1])
+                
+                arg_queries = [self.quotes.q.body.contains(string)
+                               for string in strings]
+                
+                # Whittle down the set of quotes containing the
+                # specified tags.  This is not particularly efficient,
+                # but the SQLObject contortions that would be involved
+                # in an entirely query-based solution make my head hurt.
+                tagged_quotes = set()
+                
+                for tag in tags:
+                    tag_quotes_query = ('SELECT quote_id FROM {0}quote_tag '
+                                        'LEFT JOIN {0}tags '
+                                        'ON {0}quote_tag.tag_id = {0}tags.id '
+                                        'WHERE {0}tags.tag = {1}') \
+                                       .format(self.prefix,
+                                               self.connection.sqlrepr(tag))
+                    results = set((quote[0] for quote in
+                                   self.connection.queryAll(tag_quotes_query)))
+                    
+                    if tagged_quotes:
+                        tagged_quotes &= results
+                    else:
+                        tagged_quotes = results
+                
+                if tagged_quotes:
+                    tag_query = sqlobject.OR(*[self.quotes.q.id==quote_id
+                                               for quote_id in tagged_quotes])
+                    arg_queries.append(tag_query)
+                
+                query = sqlobject.AND(self.quotes.q.approved==1, *arg_queries)
         
-        quotes = self.table.select(q_expression)
-        if quotes.count() > 0:
-            quote = random.choice(list(quotes))
-            quote_text = quote.body.replace('\n', ' ')
-            bot.reply(reply_target, channel, 'QDB: (#%d, %+d/%d) %s'
-                                              % (quote.id, quote.rating,
-                                                 quote.votes, quote_text))
-        else:
+        quotes = list(self.quotes.select(query))
+        
+        if not quotes:
             bot.reply(prefix, channel, "QDB: Couldn't find any quotes!")
+            return
+        
+        quote = random.choice(quotes)
+        quote_text = quote.body.replace('\n', ' ')
+        truncated_quote_text = util.truncate_unicode(quote_text.decode('utf-8'),
+                                                     MAX_QUOTE_CHAR_LENGTH,
+                                                     MAX_QUOTE_BYTE_LENGTH,
+                                                     self.factory.encoding)
+        
+        if quote_text != truncated_quote_text:
+            truncated_quote_text = '%s...' % truncated_quote_text
+        
+        reply = 'QDB: (#%d, %+d/%d) %s' % (quote.id, quote.rating,
+                                           quote.votes, truncated_quote_text)
+
+        if self.quote_url:
+            reply += (u' \u2014 '.encode(self.factory.encoding) +
+                      self.quote_url % quote.id)
+        
+        bot.reply(reply_target, channel, reply)
 
 
 chirpycommand = ChirpyCommand()
