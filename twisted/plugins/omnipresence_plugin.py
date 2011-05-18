52a9c8a7fa80dea744813511cd076ecf3883369f
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2011-05-18 15:34:31 -0400

    Set locale at beginning of Twisted plugin service maker

--- twisted/plugins/omnipresence_plugin.py
+++ twisted/plugins/omnipresence_plugin.py
@@ -1,12 +1,12 @@
-from zope.interface import implements
-
-from twisted.application.service import IServiceMaker
-from twisted.application import internet
-from twisted.internet import ssl
-from twisted.python import usage
-from twisted.plugin import IPlugin
-
+import locale
 import os
+
+from twisted.application import internet
+from twisted.application.service import IServiceMaker
+from twisted.internet import ssl
+from twisted.plugin import IPlugin
+from twisted.python import usage
+from zope.interface import implements
 
 from omnipresence import IRCClientFactory
 from omnipresence.config import OmnipresenceConfigParser
@@ -28,6 +28,8 @@ class ServiceMaker(object):
         Construct a TCP or SSL client from IRCClientFactory, using the options 
         in the given configuration file.
         """
+
+        locale.setlocale(locale.LC_ALL, '')
 
         config = OmnipresenceConfigParser()
         config.read(os.path.join(os.getcwd(), options['config_file']))
