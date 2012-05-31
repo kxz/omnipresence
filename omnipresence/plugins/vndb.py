4993577acae637a613b4db241e3a7c27abe5e4c0
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2012-05-31 12:04:06 -0400

    vndb: Reimplement using omnipresence.web
    --HG--
    branch : twisted.web

--- omnipresence/plugins/vndb.py
+++ omnipresence/plugins/vndb.py
@@ -1,87 +1,80 @@
-import urllib
+import urlparse
 
 from BeautifulSoup import BeautifulSoup
 from twisted.plugin import IPlugin
 from zope.interface import implements
 
-from omnipresence import html, util
-from omnipresence.iomnipresence import ICommand
+from omnipresence.web import textify_html as txt, WebCommand
 
-class VNDBSearch(object):
+
+class VisualNovelSearch(WebCommand):
     """
     \x02%s\x02 \x1Fsearch_string\x1F - Search for a visual novel with
     the given name on VNDB <http://vndb.org/>.
     """
-    implements(IPlugin, ICommand)
     name = 'vndb'
-    
-    def reply_with_result(self, response, bot, prefix,
-                          reply_target, channel, args):
-        soup = BeautifulSoup(response[1])
-        results = soup.find('div', 'mainbox browse vnbrowse')
-        
-        if not results:
-            # We were redirected to a visual novel page, as there is
-            # only one result for the given search string.
-            url = response[0]['content-location']
+    arg_type = 'a search query'
+    url = 'http://vndb.org/v/all?s=pop&o=d&q=%s'  # popularity, descending
 
+    def reply(self, response, bot, prefix, reply_target, channel, args):
+        url = response[0]['X-Omni-Location']
+        soup = BeautifulSoup(response[1])
+        vnbrowse = soup.find('div', 'mainbox browse vnbrowse')
+        results = []
+
+        # Either zero results, or more than one result.
+        if vnbrowse:
+            # Don't recurse into children, to avoid picking up the row
+            # contained within the <thead> element.
+            for row in vnbrowse.table.findAll('tr', recursive=False):
+                row_data = row.findAll('td')
+                vn = { 'url': urlparse.urljoin(url, row_data[0].a['href']),
+                       'title': txt(row_data[0].a),
+                       'release_date': txt(row_data[3]) }
+                alt_title = row_data[0].a['title']
+                if alt_title != vn['title']:
+                    vn['alt_title'] = alt_title
+                rating = txt(row_data[5], format_output=False)
+                if not rating.startswith(u'0.00'):
+                    vn['rating'] = rating.replace('(', ' (')
+                results.append(vn)
+        # Single result, redirecting to an individual visual novel page.
+        else:
             data = soup.find('div', id='maincontent')
-            title = html.textify_html(data.find('h1'))
+            vn = { 'url': url,
+                   'title': txt(data.find('h1')),
+                   'release_date': txt(data.find('td', 'tc1')) }
             alt_title = data.find('h2', 'alttitle')
-            alt_title = (u' (\x02%s\x02)' % html.textify_html(alt_title)
-                         if alt_title else u'')
-            release_date = html.textify_html(data.find('td', 'tc1'))
+            if alt_title:
+                vn['alt_title'] = txt(alt_title)
             votestats = data.find('div', 'votestats')
             if votestats:
-                rating = html.textify_html(votestats.findAll('p')[-1])
+                rating = txt(votestats.findAll('p')[-1])
                 rating = rating.rsplit(None, 1)[-1]
-
-                votes = html.textify_html(votestats.tfoot.find('td',
-                                                               colspan='2'))
+                votes = txt(votestats.tfoot.find('td', colspan='2'))
                 votes = votes.split(None, 1)[0]
+                vn['rating'] = u'{0} ({1})'.format(rating, votes)
+            results.append(vn)
 
-                rating = u'%s (%s)' % (rating, votes)
-            else:
-                rating = u''
-        elif results.thead.nextSibling:
-            data = results.thead.nextSibling.findAll('td')
-            url = 'http://vndb.org' + data[0].a['href']
-            title = html.textify_html(data[0].a)
-            alt_title = data[0].a['title']
-            alt_title = u'' if (alt_title == title) else (u' (\x02%s\x02)'
-                                                           % alt_title)
-            release_date = html.textify_html(data[3])
-            rating = html.textify_html(data[5])
-            if rating == u'0.00 (0)':
-                rating = u''
-        else:
+        if not results:
             bot.reply(prefix, channel, 'VNDB: No results found for '
                                        '\x02%s\x02.' % args[1])
             return
-        
-        if release_date == u'TBA':
-            release_date = u''
-        else:
-            release_date = u', first release %s' % release_date
-        if rating:
-            rating = u' \u2014 rated %s' % rating
 
-        bot.reply(reply_target, channel,
-                  u'VNDB: \x02%s\x02%s%s%s \u2014 %s'
-                    % (title, alt_title, release_date, rating, url))
-    
-    def execute(self, bot, prefix, reply_target, channel, args):
-        args = args.split(None, 1)
-        
-        if len(args) < 2:
-            bot.reply(prefix, channel, 'Please specify a search string.')
-            return
-        
-        params = urllib.urlencode({'q': args[1], 's': 'pop', 'o': 'd'})
-                                               # popularity, descending
-        d = self.factory.get_http('http://vndb.org/v/all?' + params)
-        d.addCallback(self.reply_with_result, bot, prefix, reply_target, channel, args)
-        return d
+        messages = []
+        for i, vn in enumerate(results):
+            message = u'VNDB ({0}/{1}): \x02{2}\x02'.format(
+                        i + 1, len(results), vn['title'])
+            if 'alt_title' in vn:
+                message += u' (\x02{0}\x02)'.format(vn['alt_title'])
+            if vn['release_date'] != u'TBA':
+                message += u', first release ' + vn['release_date']
+            if 'rating' in vn:
+                message += u' \u2014 rated ' + vn['rating']
+            message += u' \u2014 ' + vn['url']
+            messages.append(message)
+
+        bot.reply(reply_target, channel, u'\n'.join(messages))
 
 
-vndb = VNDBSearch()
+vndb = VisualNovelSearch()
