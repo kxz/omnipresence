b8b05a1875835f96477e98cfc97dedf1201c5e05
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2012-06-05 12:25:56 -0400

    Remove copyright years, as they're annoying to update

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Omnipresence'
-copyright = u'2011, Kevin Xiwei Zheng'
+copyright = u'Kevin Xiwei Zheng'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
