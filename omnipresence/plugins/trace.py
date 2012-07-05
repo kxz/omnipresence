df385259a7994c1a64277ba4e6f1c1da09656b39
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2012-07-05 16:34:22 -0400

    trace: Various improvements
    * Combine the command and handler plugins into one class
    
    * Don't bother keeping track of change times; nobody used them
    
    * Simplify the output to just a sorted list of seen nicks

--- omnipresence/plugins/trace.py
+++ omnipresence/plugins/trace.py
@@ -1,6 +1,3 @@
-import datetime
-import itertools
-
 from twisted.plugin import IPlugin
 from zope.interface import implements
 
@@ -9,36 +6,42 @@ from omnipresence.iomnipresence import ICommand, IHandler
 
 
 class NickTracer(object):
-    implements(IPlugin, IHandler)
+    """
+    \x02%s\x02 \x1Fnick\x1F - List the other nicks that the user with
+    the given nick has been seen using since the bot's or that user's
+    last join, whichever comes later.
+    """
+    implements(IPlugin, IHandler, ICommand)
     name = 'trace'
-    
+
     traces = {}
-    
+
     def endNames(self, bot, channel):
         for name in bot.channel_names[channel]:
             self.userJoined(bot, name, channel)
-    
+
     def userRenamed(self, bot, oldname, newname):
         c_oldname = ircutil.canonicalize(oldname)
         c_newname = ircutil.canonicalize(newname)
-        
+
         if c_oldname not in self.traces:
-            self.traces[c_oldname] = [(oldname, datetime.datetime.now())]
-        
+            self.traces[c_oldname] = set()
+
         if c_oldname != c_newname:
             self.traces[c_newname] = self.traces[c_oldname]
-            self.traces[c_newname].append((newname, datetime.datetime.now()))
+            self.traces[c_newname].add(oldname)
+            self.traces[c_newname].discard(newname)
             del self.traces[c_oldname]
-    
+
     def userJoined(self, bot, prefix, channel):
         nick = prefix.split('!', 1)[0]
         c_nick = ircutil.canonicalize(nick)
-        
-        # Make sure that the user isn't already in self.traces, in 
-        # case said user is already in another channel where nick 
+
+        # Make sure that the user isn't already in self.traces, in
+        # case said user is already in another channel where nick
         # tracing is enabled.
         if c_nick not in self.traces:
-            self.traces[c_nick] = [(nick, datetime.datetime.now())]
+            self.traces[c_nick] = set()
 
     def joined(self, bot, prefix, channel):
         self.userJoined(bot, bot.nickname, channel)
@@ -48,100 +51,52 @@ class NickTracer(object):
 
     def userLeft(self, bot, prefix, channel):
         nick = prefix.split('!', 1)[0]
-        
-        # Is the leaving user left in any other channels?  If not, 
-        # delete that user's record.
+
+        # Is the departing user left in any other channels?
+        # If not, delete that user's record.
         if not filter(lambda x: x[0] != channel and nick in x[1],
                       bot.channel_names.iteritems()):
             del self.traces[ircutil.canonicalize(nick)]
-    
+
     def userQuit(self, bot, prefix, quitMessage):
         c_nick = ircutil.canonicalize(prefix.split('!', 1)[0])
         del self.traces[c_nick]
 
 
-class TraceCommand(object):
-    """
-    \x02%s\x02 \x1Fnick\x1F - List the other nicks that the user with 
-    the given nick has been seen using since the bot's or that user's 
-    last join, whichever comes later.
-    """
-    implements(IPlugin, ICommand)
-    name = 'trace'
-    
     def execute(self, bot, prefix, reply_target, channel, args):
-        # Try to latch on to a NickTracer instance.
-        tracer = []
-
-        if channel == bot.nickname:
-            tracer = [filter(lambda x: isinstance(x, NickTracer),
-                             handlers) for handlers
-                                       in self.factory.handlers.itervalues()]
-            tracer = list(itertools.chain(*tracer))
-        elif channel in self.factory.handlers:
-            tracer = filter(lambda x: isinstance(x, NickTracer),
-                            self.factory.handlers[channel])
-        
-        if not tracer:
-            bot.reply(prefix, channel, 'User nicknames are not being tracked '
-                                       'in %s.' % channel)
-            return
-        
-        tracer = tracer[0]
-        
         args = args.split(None, 1)
-        
+
         if len(args) < 2:
             bot.reply(prefix, channel, 'Please specify a nickname to look up.')
             return
-        
+
         nick = args[1]
         canonicalized_nick = ircutil.canonicalize(nick)
-        
-        if canonicalized_nick == ircutil.canonicalize(bot.nickname):
-            bot.reply(prefix, channel,
-                      '%s is right here responding to your queries!'
-                       % bot.nickname)
-            return
-        
-        if canonicalized_nick not in tracer.traces:
-            bot.reply(prefix, channel, 'No user with the nick %s is currently '
-                                       'visible.' % nick)
-            return
-        
-        trace = tracer.traces[canonicalized_nick]
-        
-        if len(trace) == 1:
-            bot.reply(reply_target, channel, '%s has not changed nick since '
-                                             'first being seen %s.'
-                                              % (trace[0][0],
-                                                 util.ago(trace[0][1])))
-            return
-        
-        messages = [
-            '%s changed to that nick %s' % (trace[-1][0],
-                                            util.ago(trace[-1][1])),
-            'was first seen with the nick %s %s' % (trace[0][0],
-                                                    util.ago(trace[0][1]))
-        ]
-        
-        if len(trace) > 2:
-            trace_messages = []
-        
-            # [-2:0:-1]: start with the previous nick (-1 is current nick, -2
-            # is the one before it), and stop before reaching the first nick.
-            for record in trace[-2:0:-1]:
-                trace_messages.append('%s (changed to %s)'
-                                       % (record[0], util.ago(record[1])))
-            
-            trace_message = 'has additionally used the nick'
-            if len(trace_messages) > 1:
-                trace_message += 's'
-            trace_message += ' ' + util.andify(trace_messages)
-            messages.append(trace_message)
-        
-        bot.reply(reply_target, channel, util.andify(messages, True) + '.')
-            
 
-trace_h = NickTracer()
-trace_c = TraceCommand()
+        if canonicalized_nick == ircutil.canonicalize(bot.nickname):
+            bot.reply(prefix, channel, '{0} is right here responding to your '
+                                       'queries!'.format(bot.nickname))
+            return
+
+        if canonicalized_nick not in self.traces:
+            bot.reply(prefix, channel, 'No user with the nick {0} is currently '
+                                       'visible.'.format(nick))
+            return
+
+        trace = self.traces[canonicalized_nick]
+
+        if len(trace) == 0:
+            bot.reply(reply_target, channel, '{0} has not changed nick since '
+                                             'first being seen.'.format(nick))
+            return
+
+        message = '{0} has also used the nick'.format(nick)
+        if len(trace) > 1:
+            message += 's'
+        message += ' ' + util.andify(['\x02{0}\x02'.format(nick)
+                                      for nick in sorted(trace)])
+
+        bot.reply(reply_target, channel, message + '.')
+
+
+default = NickTracer()
