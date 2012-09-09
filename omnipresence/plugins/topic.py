fcb55974ef6358a5b84ebf47873ffee7874072e8
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2012-09-08 23:37:33 -0400

    topic: Change from prepending to single message

--- omnipresence/plugins/topic.py
+++ omnipresence/plugins/topic.py
@@ -3,57 +3,47 @@ from twisted.words.protocols import irc
 from zope.interface import implements
 
 from omnipresence import util
-from omnipresence.iomnipresence import ICommand, IHandler
+from omnipresence.iomnipresence import ICommand
 
 
-# TODO: Make these configurable.
-MAX_MESSAGE_LENGTH = 64
-MAX_TOPIC_CHAR_LENGTH = 192
-MAX_TOPIC_BYTE_LENGTH = 384
+# TODO: Make this configurable.
+MAX_MESSAGE_LENGTH = 72
 
 
 class TopicUpdater(object):
     """
-    \x02%s\x02 \x1Fstring\x1F - Add something to the channel topic,
-    assuming the bot has permission to do so.
+    \x02%s\x02 \x1Fstring\x1F - Change the channel topic.
     """
-    implements(IPlugin, ICommand, IHandler)
+    implements(IPlugin, ICommand)
     name = 'topic'
 
-    topics = {}
-    prefixes = {}
+    """Stores the format strings for each channel."""
+    formats = None
+
+    def __init__(self):
+        self.formats = {}
 
     def registered(self):
         if self.factory.config.has_section('topic'):
-            for (channel, prefix) in self.factory.config.items('topic'):
+            for (channel, format) in self.factory.config.items('topic'):
                 if channel[0] not in irc.CHANNEL_PREFIXES:
                     channel = '#%s' % channel
-                self.prefixes[channel] = prefix
-
-    def topicUpdated(self, bot, nick, channel, newTopic):
-        self.topics[channel] = newTopic
+                self.formats[channel] = format
 
     def execute(self, bot, prefix, reply_target, channel, args):
         args = args.split(None, 1)
 
         if len(args) < 2:
-            bot.reply(prefix, channel, 'Please specify a message to add.')
+            bot.reply(prefix, channel, 'Please specify a message.')
             return
 
-        if channel not in self.topics:
-            bot.reply(prefix, channel, 'Topic messages are not being tracked '
-                                       'in %s.' % channel)
+        if channel not in self.formats:
+            bot.reply(prefix, channel, 'Topic changes are not enabled in %s.'
+                                        % channel)
             return
 
-        message = ''
-
         try:
-            message = self.topics[channel].decode(self.factory.encoding)
-        except UnicodeDecodeError:
-            pass
-
-        try:
-            new_message = args[1].decode(self.factory.encoding)
+            message = args[1].decode(self.factory.encoding)
         except UnicodeDecodeError:
             bot.reply(prefix, channel, 'Topic messages must use the "%s" '
                                        'character encoding. Check your '
@@ -61,36 +51,16 @@ class TopicUpdater(object):
                                         % self.factory.encoding)
             return
 
-        if len(new_message) > MAX_MESSAGE_LENGTH:
+        message = u'<%s> %s' % (prefix.split('!', 1)[0], message)
+
+        if len(message) > MAX_MESSAGE_LENGTH:
             bot.reply(prefix, channel, 'Topic messages may not exceed %d '
                                        'characters in length.'
                                         % MAX_MESSAGE_LENGTH)
             return
 
-        messages = []
-
-        if message:
-            messages = message.split(u' | ')
-
-        messages.insert(0, u'%s (%s)' % (new_message,
-                                         prefix.split('!', 1)[0]))
-
-        if channel in self.prefixes:
-            while self.prefixes[channel] in messages:
-                messages.remove(self.prefixes[channel])
-            messages.insert(0, self.prefixes[channel])
-
-        topic = u' | '.join(messages)
-        encoded_topic = topic.encode(self.factory.encoding)
-        truncated_topic = util.truncate_unicode(topic,
-                                                MAX_TOPIC_CHAR_LENGTH,
-                                                MAX_TOPIC_BYTE_LENGTH,
-                                                self.factory.encoding)
-
-        if encoded_topic != truncated_topic:
-            truncated_topic = '%s...' % truncated_topic
-
-        bot.topic(channel, truncated_topic)
+        topic = self.formats[channel] % message
+        bot.topic(channel, topic.encode(self.factory.encoding))
 
 
 topic = TopicUpdater()
