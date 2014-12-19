e85f85249d6e01b6ef3943e50105f176b66e92a4
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2014-12-19 15:53:52 -0500

    google: Migrate to Custom Search API

--- omnipresence/plugins/google.py
+++ omnipresence/plugins/google.py
@@ -3,34 +3,37 @@ import json
 
 from bs4 import BeautifulSoup
 
-from omnipresence import web
+from omnipresence.web import WebCommand, textify_html as txt
 
 
-class GoogleSearch(web.WebCommand):
+class GoogleSearch(WebCommand):
     """
-    \x02%s\x02 \x1Fsearch_string\x1F - Perform a Google search on the given
-    search string.
+    \x02%s\x02 \x1Fsearch_string\x1F - Perform a Google search on the
+    given search string.
     """
     name = 'google'
     arg_type = 'a search query'
-    url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=%s'
+    url = 'https://www.googleapis.com/customsearch/v1?q=%s'
+
+    def registered(self):
+        self.url += '&key=%s&cx=%s' % (
+            self.factory.config.get('google', 'key'),
+            self.factory.config.get('google', 'cx'))
 
     def reply(self, response, bot, prefix, reply_target, channel, args):
         data = json.loads(response[1])
-        try:
-            results = data['responseData']['results']
-        except KeyError:
-            results = []
-        if not results:
+        if 'error' in data:
+            raise IOError('Google API error: ' + data['error']['message'])
+        items = data.get('items')
+        if not items:
             bot.reply(prefix, channel,
                       'Google: No results found for \x02%s\x02.' % args[1])
             return
         bot.reply(reply_target, channel, u'\n'.join(
-                    u'Google: ({0}/{1}) {2} \u2014 \x02{3}\x02: {4}'.format(
-                      i + 1, len(results), result['unescapedUrl'],
-                      web.textify_html(result['titleNoFormatting']),
-                      web.textify_html(BeautifulSoup(result['content'])))
-                    for i, result in enumerate(results)))
+                    u'Google: ({}/{}) {} \u2014 \x02{}\x02: {}'.format(
+                      i + 1, len(items), item['link'], item['title'],
+                      txt(BeautifulSoup(item['htmlSnippet'])))
+                    for i, item in enumerate(items)))
 
 
 default = GoogleSearch()
