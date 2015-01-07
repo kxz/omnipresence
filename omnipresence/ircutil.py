89422718ab69252aabfe83fc07651c1a2bded7ef
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2015-01-07 18:30:46 -0500

    Deprecate superseded ircutil methods

--- omnipresence/ircutil.py
+++ omnipresence/ircutil.py
@@ -1,50 +1,34 @@
-# -*- test-case-name: omnipresence.test.test_ircutil -*-
-"""IRC-specific utility functions."""
+"""IRC-specific utility functions.
+
+Most of the functions in this module are deprecated, having been
+superseded by more powerful APIs elsewhere in Omnipresence.
+"""
+
+
 import re
 
-# Common IRC formatting control codes
-# <http://forum.egghelp.org/viewtopic.php?p=94834>
-# <http://www.mirc.com/help/colors.html>
-CONTROL_CODES = re.compile(r"""
-(
-  \x02              # Bold
-  |
-  \x03(             # Color
-    [0-9]?[0-9](    # Optional foreground number (from 0 or 00 to 99)
-      ,[0-9]?[0-9]  # Optional background number (from 0 or 00 to 99)
-    )?
-  )?
-  |
-  \x0F              # Normal (revert to default formatting)
-  |
-  \x16              # Reverse video (sometimes rendered as italics)
-  |
-  \x1F              # Underline
-)
-""", re.VERBOSE)
+from . import mapping
+from .hostmask import Hostmask
+from .message import remove_formatting, unclosed_formatting
 
 
 def canonicalize(name, casemapping='rfc1459'):
     """Convert an IRC name to its "canonical" lowercase representation.
     The *casemapping* parameter determines the case folding rules used
-    to perform the conversion:
+    to perform the conversion.  It takes the same values as the *name*
+    parameter to :py:meth:`~.mapping.by_name`, defaulting to
+    ``'rfc1459'``.  If the value of *casemapping* is not one of the
+    above values, it is treated as the default.
 
-    * ``'ascii'`` treats the letters *A-Z* as uppercase versions of the
-      letters *a-z*.
-    * ``'strict-rfc1459'`` uses the ``'ascii'`` case mappings, further
-      treating *{}|* as the lowercase versions of *[]\*.
-    * ``'rfc1459'`` uses the ``'strict-rfc1459'`` case mappings, further
-      treating *~* as the lowercase version of *^*.
+    .. deprecated:: 2.4
+       Use the :py:mod:`~.mapping` API instead.
+    """
+    try:
+        cm = mapping.by_name(casemapping)
+    except ValueError:
+        cm = mapping.by_name('rfc1459')
+    return cm.lower(name)
 
-    The default value is ``'rfc1459'``.  If the value of *casemapping*
-    is not one of the above values, it is treated as the default."""
-    name = name.lower()
-    if casemapping == 'ascii':
-        return name
-    name = name.replace('[',  '{').replace(']',  '}').replace('\\', '|')
-    if casemapping == 'strict-rfc1459':
-        return name
-    return name.replace('^',  '~')
 
 def mode_string(set, modes, args):
     """Given the arguments "set", "modes", and "args" as passed to
@@ -57,58 +41,18 @@ def mode_string(set, modes, args):
     return (('+' if set else '-') +
             modes + (' ' + ' '.join(args) if args else ''))
 
-def parse_hostmask(hostmask):
-    """Turn a hostmask in the form *nick!user@host* into a 3-tuple
-    ``(nick, user, host)``, with ``None`` appearing in place of any
-    components not present in the original hostmask."""
-    nick, _, rest = hostmask.partition('!')
-    user, _, host = rest.partition('@')
-    return (nick, user or None, host or None)
 
-def remove_control_codes(s):
-    """Remove IRC formatting control codes from a string."""
-    return CONTROL_CODES.sub('', s)
+# Aliases for methods that have simply been moved elsewhere.
+parse_hostmask = Hostmask.from_string
+remove_control_codes = remove_formatting
+unclosed_formatting_codes = unclosed_formatting
 
-def unclosed_formatting_codes(s):
-    """Return a list of the unclosed IRC formatting codes in a string."""
-    fg = bg = ''
-    bold = reverse = underline = False
-    # ^O resets everything, so we split on it and only operate on the
-    # portion of the string that is beyond the rightmost occurrence.
-    for match in CONTROL_CODES.finditer(s.rsplit('\x0F')[-1]):
-        code = match.group(0)
-        if code.startswith('\x03'):
-            # Foreground and background colors can be set separately as
-            # long as at least one is specified.
-            if code[1:]:
-                new_fg, _, new_bg = code[1:].partition(',')
-                fg = new_fg or fg
-                bg = new_bg or bg
-            # Nothing was specified; reset the whole thing.
-            else:
-                fg = bg = ''
-        elif code == '\x02':
-            bold = not bold
-        elif code == '\x16':
-            reverse = not reverse
-        elif code == '\x1F':
-            underline = not underline
-    # Thankfully, we don't have to keep track of proper nesting.
-    open_codes = []
-    if fg or bg:
-        open_codes.append('\x03%s,%s' % (fg, bg))
-    if bold:
-        open_codes.append('\x02')
-    if reverse:
-        open_codes.append('\x16')
-    if underline:
-        open_codes.append('\x1F')
-    return open_codes
 
 def close_formatting_codes(s):
-    """Return the given string with all open formatting codes closed."""
-    open_codes = unclosed_formatting_codes(s)
-    for i in xrange(len(open_codes)):
-        if open_codes[i].startswith('\x03'):
-            open_codes[i] = '\x03'
-    return s + ''.join(open_codes)
+    """Return the given string with all open formatting codes closed.
+
+    .. deprecated:: 2.4
+       This function simply appends ``^O`` to the end of *s*.  Older
+       implementations were overly complex and bug-prone.
+    """
+    return s + '\x0F'
