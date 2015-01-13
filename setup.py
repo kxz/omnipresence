6a57a09631d190939c80b77ba268c2ee7761a394
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2015-01-12 21:45:28 -0500

    Correct dependency listings

--- setup.py
+++ setup.py
@@ -33,12 +33,11 @@ setup(
         'Twisted>=14.0.0',
         'pyOpenSSL',
         'service_identity',
-        'sqlobject>=0.10'],
+        'SQLObject>=0.10'],
     extras_require={
         'html': [
             'beautifulsoup4']},
     tests_require=[
-        'beautifulsoup4',
         'tox'],
     cmdclass={
         'test': Tox},
