83b1e7b313da750157e4ae1431abf081953ea116
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2015-02-01 14:53:56 -0500

    Stub implementation of help plugin

---
+++ omnipresence/plugins/help/test.py
@@ -0,0 +1,10 @@
+"""Unit tests for the help event plugin."""
+# pylint: disable=missing-docstring,too-few-public-methods
+
+from ..test.helpers import AbstractConnectionTestCase
+
+from . import default
+
+
+class HelpTestCase(AbstractConnectionTestCase):
+    pass
