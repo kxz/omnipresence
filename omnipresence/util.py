ae6f7a0e15fde9e4be270773e94d9181462c4f50
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2015-02-02 15:22:13 -0500

    Remove deprecated .util module

--- omnipresence/util.py
+++
@@ -1,9 +0,0 @@
-"""General utility functions used within Omnipresence.
-
-.. deprecated:: 2.4
-    The majority of this module's functions have moved to the much more
-    concretely named :py:mod:`~.humanize`.
-"""
-
-
-from .humanize import ago, andify, duration_to_timedelta, readable_duration
