eafba6f91a44c451aac3bab397c9ab262b6ed7d4
Kevin Xiwei Zheng <blankplacement+omnipresence@gmail.com>
2015-07-30 21:51:42 -0400

    url: Spin off title fetching to Little Brother
    This makes the plugin code a bit less hairy and makes the parts that
    aren't specific to Omnipresence easier to use as their own package.

--- omnipresence/plugins/url/__init__.py
+++ omnipresence/plugins/url/__init__.py
@@ -1,73 +1,22 @@
+"""Omnipresence plugins for URL title fetching."""
 # -*- test-case-name: omnipresence.plugins.url.test_url
-import cgi
-from collections import namedtuple
+
+
 import re
-import socket
-import sys
 import urllib
-import urlparse
+from urlparse import urlparse, urldefrag
 
-import ipaddress
-from twisted.internet import defer, reactor, protocol, threads
+from littlebrother import fetch_title
+from twisted.internet.defer import DeferredList, inlineCallbacks, returnValue
 from twisted.internet.error import ConnectError, DNSLookupError
-from twisted.plugin import getPlugins, pluginPackagePaths, IPlugin
+from twisted.plugin import IPlugin
 from twisted.python import log
-from twisted.python.failure import Failure
-from twisted.web.client import (IAgent, Agent, ContentDecoderAgent,
-                                RedirectAgent, GzipDecoder, ResponseFailed)
+from twisted.web.client import ResponseFailed
 from twisted.web.error import InfiniteRedirection
-from twisted.web.iweb import UNKNOWN_LENGTH
-from zope.interface import implements, Interface, Attribute
+from zope.interface import implements
 
-from omnipresence import plugins, web
-from omnipresence.iomnipresence import IHandler
+from ...iomnipresence import IHandler
 
-
-# Make twisted.plugin.getPlugins() look for plug-ins in this module.
-__path__.extend(pluginPackagePaths(__name__))
-__all__ = []
-
-
-"""The maximum number of bytes the fetcher will download for a single
-title fetch request."""
-MAX_DOWNLOAD_SIZE = 65536
-
-"""The maximum number of "soft" redirects that will be followed."""
-MAX_SOFT_REDIRECTS = 2
-
-
-#
-# Utility methods
-#
-
-def add_si_prefix(number, unit, plural_unit=None):
-    """Returns a string containing an approximate representation of the
-    given number and unit, using a decimal SI prefix.  *number* is
-    assumed to be a positive integer. If *plural_unit* is not specified,
-    it defaults to *unit* with an "s" appended."""
-    if not plural_unit:
-        plural_unit = unit + 's'
-
-    if number == 1:
-        return '{0:n} {1}'.format(number, unit)
-
-    prefix = ''
-    for prefix in ('', 'kilo', 'mega', 'giga', 'tera',
-                   'exa', 'peta', 'zetta', 'yotta'):
-        if number < 1000:
-            break
-
-        number /= 1000.0
-
-    if prefix:
-        return '{0:.3n} {1}{2}'.format(number, prefix, plural_unit)
-
-    return '{0:n} {1}'.format(number, plural_unit)
-
-
-#
-# Utility methods
-#
 
 # Based on django.utils.html.urlize from the Django project.
 TRAILING_PUNCTUATION = ['.', ',', ':', ';', '.)', '"', "'", '!']
@@ -102,218 +51,63 @@ def extract_urls(text):
             yield middle
 
 
-#
-# Twisted HTTP machinery
-#
-
-class TruncatingReadBodyProtocol(protocol.Protocol):
-    """A protocol that collects data sent to it up to a maximum of
-    *max_bytes*, then discards the rest."""
-
-    def __init__(self, status, message, finished, max_bytes=None):
-        self.status = status
-        self.message = message
-        self.finished = finished
-        self.data_buffer = []
-        self.remaining = max_bytes or sys.maxsize
-
-    def dataReceived(self, data):
-        if self.remaining > 0:
-            to_buffer = data[:self.remaining]
-            self.data_buffer.append(to_buffer)
-            self.remaining -= len(to_buffer)
-        if self.remaining <= 0:
-            self.transport.loseConnection()
-
-    def connectionLost(self, reason):
-        if not self.finished.called:
-            self.finished.callback(''.join(self.data_buffer))
-
-
-class BlacklistedHost(Exception):
-    """Raised when a `BlacklistingAgent` attempts to request a
-    blacklisted resource."""
-
-    def __init__(self, hostname, ip):
-        self.hostname = hostname
-        self.ip = ip
-
-    def __str__(self):
-        return 'host {} corresponds to blacklisted IP {}'.format(
-            self.hostname, self.ip)
-
-
-class BlacklistingAgent(object):
-    """An `~twisted.web.client.Agent` wrapper that forbids requests to
-    loopback, private, and internal IP addresses."""
-    implements(IAgent)
-
-    def __init__(self, agent, resolve=None):
-        self.agent = agent
-        self.resolve = resolve or reactor.resolve
-
-    @defer.inlineCallbacks
-    def request(self, method, uri, headers=None, bodyProducer=None):
-        """Issue a request to the server indicated by *uri*."""
-        hostname = urlparse.urlparse(uri).hostname
-        ip_str = yield self.resolve(hostname)
-        # `ipaddress` takes a Unicode string and I don't really care to
-        # handle `UnicodeDecodeError` separately.
-        ip = ipaddress.ip_address(ip_str.decode('ascii', 'replace'))
-        if ip.is_private or ip.is_loopback or ip.is_link_local:
-            raise BlacklistedHost(hostname, ip)
-        response = yield self.agent.request(method, uri, headers, bodyProducer)
-        defer.returnValue(response)
-
-agent = ContentDecoderAgent(
-    RedirectAgent(BlacklistingAgent(Agent(reactor))),
-    [('gzip', GzipDecoder)])
-
-
-#
-# Plugin interfaces and helper classes
-#
-
-#: Returned by title processors to indicate a "soft" redirect, such as
-#: an HTML ``<meta>`` refresh.  The *location* parameter indicates the
-#: new URL to fetch.
-Redirect = namedtuple('Redirect', ['location'])
-
-
-class ITitleProcessor(Interface):
-    """A plugin that can retrieve the title of a given Web document."""
-
-    supported_content_types = Attribute("""
-        An iterable containing the MIME content types that this title
-        processor supports; for example, ``('image/gif', 'text/xml')``.
-        """)
-
-    def process(self, headers, content):
-        """Implement this method in your title processor class.  The
-        *headers* and *content* arguments are in the format returned by
-        :py:meth:`omnipresence.web.request()`.
-
-        This method should either return a Unicode string containing the
-        extracted title, or a :py:class:`Redirect` object pointing at a
-        new URL from which to fetch a document title.
-        """
-
-#
-# Actual handler plugin
-#
-
 class URLTitleFetcher(object):
-    """
-    Reply to messages containing URLs with an appropriate title (the
+    """Reply to messages containing URLs with an appropriate title (the
     contents of the <title> element for HTML pages, the first line for
-    plain text documents, and so forth.)
-    """
+    plain text documents, and so forth.)"""
     implements(IPlugin, IHandler)
     name = 'url'
 
     def __init__(self):
         self.ignore_list = []
-        self.title_processors = {}
 
     def registered(self):
         self.ignore_list = self.factory.config.getspacelist(
-                             'url', 'ignore_messages_from')
-        for title_processor in getPlugins(ITitleProcessor, plugins.url):
-            for content_type in title_processor.supported_content_types:
-                self.title_processors[content_type] = title_processor
+            'url', 'ignore_messages_from')
 
     def privmsg(self, bot, prefix, channel, message):
         nick = prefix.split('!', 1)[0]
-
         if nick in self.ignore_list:
             return
-
-        # Everything in here is Unicode.
-        message = message.decode(self.factory.encoding, 'ignore')
         fetchers = []
         for url in extract_urls(message):
             # Perform some basic URL format sanity checks.
-            parsed = urlparse.urlparse(url)
-            # Make sure we have a valid hostname.
+            parsed = urlparse(url)
             hostname = parsed.hostname
             if hostname is None:
                 log.msg('Could not extract hostname from URL {}; '
-                        'ignoring.'.format(url.encode('utf8')))
+                        'ignoring' % url)
                 continue
-
-            # Okay, everything looks good.  Log the seen URL.
-            log.msg('Saw URL %s from %s in channel %s.'
-                    % (url.encode('utf8'), prefix, channel))
-
-            # Strip the fragment portion of the URL, if present.
-            url, frag = urlparse.urldefrag(url)
-
+            log.msg('Saw URL %s from %s in channel %s' %
+                    (url, prefix, channel))
+            url, frag = urldefrag(url)
             # Look for "crawlable" AJAX URLs with fragments that begin
             # with "#!", and transform them to use "_escaped_fragment_".
             #
             # <http://code.google.com/web/ajaxcrawling/>
             if frag.startswith('!'):
-                url += (u'&' if u'?' in url else u'?' +
-                        u'_escaped_fragment_=' +
-                        urllib.quote(frag[1:].encode('utf8')))
-
-            # Make the actual request.
+                url += ('&' if '?' in url else '?' +
+                        '_escaped_fragment_=' + urllib.quote(frag[1:]))
             d = self.get_title(url, hostname)
             d.addErrback(self.make_error_reply, hostname)
             fetchers.append(d)
-
-        l = defer.DeferredList(fetchers)
+        l = DeferredList(fetchers)
         l.addCallback(self.reply, bot, prefix, channel)
         return l
 
     action = privmsg
 
-    @defer.inlineCallbacks
+    @inlineCallbacks
     def get_title(self, url, hostname):
-        # Fetch the title from a title processor if one is available for
-        # the response's Content-Type, or craft a default one.
-        title = None
-        for _ in xrange(MAX_SOFT_REDIRECTS):
-            response = yield agent.request('GET', url.encode('utf8'))
-            clength = response.length
-            headers = {k: v[0] for k, v in response.headers.getAllRawHeaders()}
-            headers['X-Omni-Length'] = str(clength)
-            finished = defer.Deferred()
-            response.deliverBody(TruncatingReadBodyProtocol(
-                response.code, response.phrase, finished, MAX_DOWNLOAD_SIZE))
-            content = yield finished
-            ctype, cparams = cgi.parse_header(headers.get('Content-Type', ''))
-            if ctype in self.title_processors:
-                title_processor = self.title_processors[ctype]
-                processed = yield threads.deferToThread(
-                    title_processor.process, headers, content)
-                if isinstance(processed, Redirect):
-                    # Join the new location with the current URL, in
-                    # order to handle relative URLs.
-                    url = urlparse.urljoin(url, processed.location)
-                    continue
-                title = processed
-            # The only case where we'd want to loop again is when the
-            # response returned is a soft redirect.
-            break
-        else:
-            raise ResponseFailed([Failure(InfiniteRedirection(
-                599, 'Too many soft redirects', location=url))])
-        if title is None:
-            title = u'{} document'.format(ctype or u'Unknown')
-            if clength is not UNKNOWN_LENGTH:
-                title += u' ({})'.format(add_si_prefix(clength, 'byte'))
-
+        response, title = yield fetch_title(url, with_response=True)
         # Add a hostname tag to the returned title, indicating any
         # redirects to a different host that occurred.
-        final_hostname = urlparse.urlparse(
-            response.request.absoluteURI).hostname
+        final_hostname = urlparse(response.request.absoluteURI).hostname
         if hostname != final_hostname:
             hostname_tag = u'{} \u2192 {}'.format(hostname, final_hostname)
         else:
             hostname_tag = hostname
-
-        defer.returnValue((hostname_tag, title))
+        returnValue((hostname_tag, title))
 
     def make_error_reply(self, failure, hostname):
         message = None
@@ -332,27 +126,22 @@ class URLTitleFetcher(object):
     def reply(self, results, bot, prefix, channel):
         for i, result in enumerate(results):
             success, value = result
-
             if success:
                 hostname, title = value
                 title = u'[{}] {}'.format(hostname, title)
             else:
                 log.err(value, 'Encountered an error in URL processing.')
                 title = u'Error: \x02{:s}\x02.'.format(value.value)
-
             if len(title) >= 140:
                 title = u'{}\u2026{}'.format(title[:64], title[-64:])
-
             if len(results) > 1:
                 message = u'URL ({}/{}): {}'.format(i + 1, len(results), title)
             else:
                 message = u'URL: {}'.format(title)
-
-            # In the event that we're enabled for private messages...
             if channel == bot.nickname:
                 bot.reply(prefix, channel, message)
             else:
                 bot.reply(None, channel, message)
 
 
-url = URLTitleFetcher()
+default = URLTitleFetcher()
--- omnipresence/plugins/url/test_url.py
+++ omnipresence/plugins/url/test_url.py
@@ -2,17 +2,9 @@
 # pylint: disable=missing-docstring,too-few-public-methods
 
 
-from twisted.internet.defer import Deferred, succeed
-from twisted.python.failure import Failure
-from twisted.test.proto_helpers import StringTransport
 from twisted.trial import unittest
-from twisted.web.client import Response
-from twisted.web.http_headers import Headers
-from twisted.web.test.test_agent import (AgentTestsMixin,
-                                         FakeReactorAndConnectMixin)
 
-from . import (extract_urls, TruncatingReadBodyProtocol,
-               BlacklistingAgent, BlacklistedHost)
+from . import extract_urls
 
 
 class ExtractURLsTestCase(unittest.TestCase):
@@ -72,63 +64,3 @@ class ExtractURLsTestCase(unittest.TestCase):
         self.assert_urls(
             'http://i.ebayimg.com/00/s/MTAwOFgxMDI0/$(KGrHqYOKo0E6fEy4,lqBOt,yzoor!~~60_12.JPG',
             ['http://i.ebayimg.com/00/s/MTAwOFgxMDI0/$(KGrHqYOKo0E6fEy4,lqBOt,yzoor!~~60_12.JPG'])
-
-
-class TruncatingReadBodyProtocolTestCase(unittest.TestCase):
-    def assert_delivery(self, data, expected):
-        finished = Deferred()
-        finished.addCallback(self.assertEqual, expected)
-        response = Response(('HTTP', 1, 1), 200, 'OK', Headers(),
-                            StringTransport())
-        protocol = TruncatingReadBodyProtocol(
-            response.code, response.phrase, finished, max_bytes=8)
-        response.deliverBody(protocol)
-        response._bodyDataReceived(data)
-        response._bodyDataFinished()
-        return finished
-
-    def test_complete(self):
-        return self.assert_delivery('#' * 4, '#' * 4)
-
-    def test_truncated(self):
-        return self.assert_delivery('#' * 16, '#' * 8)
-
-
-class BlacklistingAgentTestCase(unittest.TestCase,
-                                FakeReactorAndConnectMixin, AgentTestsMixin):
-    # <https://twistedmatrix.com/trac/ticket/4024>... one wishes.
-    #
-    # Based in part on `twisted.web.test.test_agent.RedirectAgentTests`.
-
-    sample_hosts = ('localhost', '0.0.0.0', '10.0.0.1', '127.0.0.1',
-                    '169.254.0.1', '172.16.0.1', '192.168.0.1')
-
-    @staticmethod
-    def resolve(hostname):
-        if hostname == 'localhost':
-            return succeed('127.0.0.1')
-        elif hostname == 'foo.test':
-            return succeed('8.8.8.8')
-        return succeed(hostname)
-
-    def makeAgent(self):
-        return BlacklistingAgent(self.buildAgentForWrapperTest(self.reactor),
-                                 resolve=self.resolve)
-
-    def setUp(self):
-        self.reactor = self.Reactor()
-        self.agent = self.makeAgent()
-
-    def test_no_blacklist(self):
-        self.agent.request('GET', 'http://foo.test/')
-
-    def assert_blacklist(self, method, uri):
-        d = self.agent.request(method, uri)
-        f = self.failureResultOf(d, BlacklistedHost)
-
-    def test_blacklist(self):
-        for protocol in ('http', 'https'):
-            for host in self.sample_hosts:
-                uri = '{}://{}/'.format(protocol, host)
-                for method in ('GET', 'POST'):
-                    self.assert_blacklist(method, uri)
